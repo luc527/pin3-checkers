adac6076f7544b30d31dfc1772f93195a133f7a9
Lucas Moraes Schwambach <lucas.schwambach52@gmail.com>
2022-09-17 17:06:08 -0300

    add heuristicClusters

--- minimax.js
+++ minimax.js
@@ -2,9 +2,9 @@ import * as s from './game-state.js'
 import * as bo from './board.js'
 
 export class Minimax {
-  constructor(maximizeWhite, heuristicFunction, cutoffDepth) {
+  constructor(maximizeWhite, valueHeuristic, cutoffDepth) {
     this.maximizeWhite = maximizeWhite
-    this.heuristicFunction = heuristicFunction
+    this.valueHeuristic = valueHeuristic
     this.cutoffDepth = cutoffDepth
     this.leafCount = 0
   }
@@ -20,7 +20,7 @@ export class Minimax {
   val(state, depth=0, alpha=-Infinity, beta=+Infinity) {
     if (depth >= this.cutoffDepth) {
       this.leafCount++
-      return { value: this.heuristicFunction(state, this.maximizeWhite) }
+      return { value: this.valueHeuristic(state, this.maximizeWhite) }
     }
 
     const actions = s.getActions(state)
@@ -102,6 +102,7 @@ export class Minimax {
   }
 }
 
+// TODO doc
 export function heuristicCountPieces(state, maximizeWhite) {
   const board = state.board
 
@@ -118,4 +119,77 @@ export function heuristicCountPieces(state, maximizeWhite) {
   }
 
   return boardValue
+}
+
+// TODO doc
+function sameColorPieceClusters(board) {
+  // for using positions as keys in the clusterOf Map
+  function k(row, col) { return row * 8 + col }
+  function p(key) { return { row: Math.floor(key / 8), col: key % 8} }
+
+  const clusterOf = new Map()
+  let clusterCount = 0
+
+  function dfs(row, col, white) {
+    clusterOf.set(k(row, col), clusterCount)
+    for (const rowOffset of [-1, 1]) {
+      for (const colOffset of [-1, 1]) {
+        // neighbour position
+        const nrow = row + rowOffset 
+        const ncol = col + colOffset
+        if (nrow < 0 || nrow > 7 || ncol < 0 || ncol > 7) continue
+
+        const nkey = k(nrow, ncol)
+        const npiece = board[nrow][ncol]
+        if (npiece && npiece.white == white && !clusterOf.has(nkey)) {
+          dfs(nrow, ncol, white)
+        }
+      }
+    }
+  }
+
+  for (let i=0; i<8; i++) {
+    for (let j=0; j<8; j++) {
+      const piece = board[i][j]
+      if (!piece) continue
+      if (clusterOf.has(k(i, j))) continue
+      dfs(i, j, piece.white)
+      clusterCount++
+    }
+  }
+
+  const clusters = Array(clusterCount)
+  for (let i=0; i<clusterCount; i++) {
+    clusters[i] = []
+  }
+
+  for (const [key, clusterId] of clusterOf.entries()) {
+    clusters[clusterId].push(p(key))
+  }
+
+  return clusters
+}
+
+// TODO doc
+export function heuristicClusters(state, maximizeWhite) {
+
+  let value = 0
+
+  for (const cluster of sameColorPieceClusters(state.board)) {
+    let pieceCountValue = 0
+    for (const {row, col} of cluster) {
+      pieceCountValue += state.board[row][col].king ? 2 : 1
+    }
+
+    // this makes clusters VERY valuable
+    // could be something like 1 + cluster.length/4 instead
+    const clusterFactor = cluster.length
+
+    const fst = cluster[0]
+    const sign = state.board[fst.row][fst.col].white == maximizeWhite ? 1 : -1
+
+    value += sign * clusterFactor * pieceCountValue
+  }
+
+  return value
 }
