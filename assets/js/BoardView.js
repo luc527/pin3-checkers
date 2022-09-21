2a3d37b0a464be39bca7ae9e21e232a17cbb07f7
Lucas Moraes Schwambach <lucas.schwambach52@gmail.com>
2022-09-20 23:34:44 -0300

    add BoardView class to replace board-render.js functions

---
+++ assets/js/BoardView.js
@@ -0,0 +1,64 @@
+export default class BoardView {
+
+  constructor(tableElem, overlayCoords = false) {
+    tableElem.setAttribute('cellspacing', 0)
+    const pieceMatrix = Array(8)
+
+    for (let i = 0; i < 8; i++) {
+      pieceMatrix[i] = Array(8)
+
+      const rowElem = document.createElement('tr')
+      rowElem.classList.add('board-row')
+
+      for (let j = 0; j < 8; j++) {
+        const cellElem = document.createElement('td');
+        cellElem.id = `${i}-${j}`
+        const cellColorClass = (i + j + 1) % 2 == 0 ? 'white' : 'black'
+        cellElem.classList.add('board-cell', cellColorClass)
+
+        const pieceElem = document.createElement('div')
+        pieceElem.classList.add('piece')
+        pieceElem.style.visibility = 'hidden'
+
+        pieceMatrix[i][j] = pieceElem
+
+        cellElem.append(pieceElem)
+
+        if (overlayCoords) {
+          const text = document.createElement('span')
+          text.classList.add('cell-text')
+          text.innerHTML = `r${i}<br>c${j}`
+          cellElem.append(text)
+        }
+
+        rowElem.append(cellElem)
+      }
+
+      tableElem.append(rowElem)
+    }
+
+    this.pieceMatrix = pieceMatrix
+  }
+
+  render(board) {
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        const elem = this.pieceMatrix[i][j]
+        const piece = board[i][j]
+
+        if (!piece) {
+          elem.style.visibility = 'hidden'
+        } else {
+          elem.style.visibility = 'visible'
+
+          elem.classList.remove(piece.king ? 'man' : 'king')
+          elem.classList.add(piece.king ? 'king' : 'man')
+
+          elem.classList.remove(piece.white ? 'black' : 'white')
+          elem.classList.add(piece.white ? 'white' : 'black')
+        }
+      }
+    }
+
+  }
+}
