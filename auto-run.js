ccbcc8f9a4a429c1f4eac0c3b40db820c9b7ec93
Lucas Moraes Schwambach <lucas.schwambach52@gmail.com>
2022-10-26 21:15:15 -0300

    feat: more functionality to config.json
    - can be either a single config or an array of configs
    - each config can have a 'runs' property (how many games to play)
    - players['white'] and players['black'] instead of config[0] and config[1]

--- auto-run.js
+++ auto-run.js
@@ -4,38 +4,58 @@ import { CheckersState, Status } from './assets/js/game-state.js'
 import { captureOptionsFromBools } from './assets/js/move-generation.js';
 
 const filePath = process.argv.slice(2)[0]
-const gameConfig = JSON.parse(readFileSync(filePath, 'utf8'))
-console.log(gameConfig)
+let gameConfigArray = JSON.parse(readFileSync(filePath, 'utf8'))
 
-const heur1 = minimax[gameConfig.config[0].function]
-const depth1 = gameConfig.config[0].depth
-const aiWhite = new minimax.Minimax(true, heur1, depth1)
-
-const heur2 = minimax[gameConfig.config[1].function]
-const depth2 = gameConfig.config[1].depth
-const aiBlack = new minimax.Minimax(false, heur2, depth2)
-
-const mandatory = gameConfig.rule == 'capturesMandatory'
-const bestMandatory = gameConfig.rule == 'bestCapturesMandatory'
-const captureOptions = captureOptionsFromBools(mandatory, bestMandatory)
-
-const state = new CheckersState(captureOptions)
-
-const randomMoves = gameConfig.random
-for (let i = 0; i < randomMoves; i++) {
-  const rand = Math.floor(Math.random() * state.actions.length)
-  const action = state.actions[rand]
-  state.actionDo(action)
+if (!Array.isArray(gameConfigArray)) {
+  gameConfigArray = [gameConfigArray];
 }
 
-while (state.status == Status.playing) {
-  const mm = state.whiteToMove ? aiWhite : aiBlack;
-  const { action } = mm.val(state)
-  state.actionDo(action)
+for (const gameConfig of gameConfigArray) {
+
+  console.log(gameConfig);
+
+  const heur1 = minimax[gameConfig.players['white'].function]
+  const depth1 = gameConfig.players['white'].depth
+  const aiWhite = new minimax.Minimax(true, heur1, depth1)
+
+  const heur2 = minimax[gameConfig.players['black'].function]
+  const depth2 = gameConfig.players['black'].depth
+  const aiBlack = new minimax.Minimax(false, heur2, depth2)
+
+  const mandatory = gameConfig.rule == 'capturesMandatory'
+  const bestMandatory = gameConfig.rule == 'bestCapturesMandatory'
+  const captureOptions = captureOptionsFromBools(mandatory, bestMandatory)
+
+  let runs = gameConfig.runs ?? 1;
+
+  // TODO more statistics
+  // - count # of moves until end of game
+  // - aggregate at the end: how many games each player won
+
+  // TODO output results as JSON to file
+
+  while (runs-- > 0) {
+    const state = new CheckersState(captureOptions)
+
+    const randomMoves = gameConfig.random
+    for (let i = 0; i < randomMoves; i++) {
+      const rand = Math.floor(Math.random() * state.actions.length)
+      const action = state.actions[rand]
+      state.actionDo(action)
+    }
+
+    while (state.status == Status.playing) {
+      const mm = state.whiteToMove ? aiWhite : aiBlack;
+      const { action } = mm.val(state)
+      state.actionDo(action)
+    }
+
+    if (state.status == Status.draw) {
+      console.log('Draw :/')
+    } else {
+      console.log(`Winner is ${state.status == Status.whiteWon ? 'white' : 'black'}`)
+    }
+  }
 }
 
-if (state.status == Status.draw) {
-  console.log('Draw :/')
-} else {
-  console.log(`Winner is ${state.status == Status.whiteWon ? 'white' : 'black'}`)
-}
+
